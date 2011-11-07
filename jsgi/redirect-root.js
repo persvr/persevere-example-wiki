50b68d91b405c02a893727ab4184c864cbb3f762
Kris Zyp <kriszyp@gmail.com>
2011-11-07 11:43:18 -0700

    Use pintura redirect module

--- jsgi/redirect-root.js
+++ jsgi/redirect-root.js
@@ -1,4 +1,4 @@
-var redirector = require("./redirect").Redirect("/Page/Example");
+var redirector = require("pintura/jsgi/redirect").Redirect("/Page/Example");
 exports.RedirectRoot = function(app){
 	return function(request){
 		if(request.pathInfo == "/"){
